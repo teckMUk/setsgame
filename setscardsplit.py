4a711f751fb2eba8ce2fea0839aa6107fa4b6b17
teckMUk <23100313@lums.edu.pk>
2020-06-14 21:33:30 +0300

    added new filed to collection

--- setscardsplit.py
+++ setscardsplit.py
@@ -5,7 +5,7 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["setsgame"]
 mycol = mydb["cards"]
 
-dictin = os.listdir("/mnt/c/Users/mukeet/Documents/setsgame")
+dictin = os.listdir("/mnt/c/Users/mukeet/Documents/setsgame_cards_image")
 mylist = []
 variable = 0
 
@@ -16,11 +16,15 @@ for filename in dictin:
         shape = filedetails[0]
         color = filedetails[1]
         fill_type = filedetails[2]
+        file_sub_detail = filedetails[3]
+        shapenumber_filetype = file_sub_detail.split(".")
+        number_of_shapes = shapenumber_filetype[0]
         mycards_details = {
             "_id": variable,
             "color": color,
             "shape": shape,
             "fill_type": fill_type,
+            "Number of shapes" : number_of_shapes,
             "file_name": filename,
         }
         mylist.append(mycards_details)
